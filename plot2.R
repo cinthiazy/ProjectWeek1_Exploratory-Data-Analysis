e3ac7442b3a4286c73dae16dc43fe985483c1c58
Cinthia Zarabia Y <cinthia.zarabia@gmail.com>
2017-10-23 23:40:37 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,19 +1,12 @@
-
-#------------------------------------#
 #-----Changing the Directory --------#
-#------------------------------------#
 rm(list=ls())
 setwd("E:/CURSOS/EspecializacionDataScience-JH/ExploratoryDataAnalysis-Coursera/Week1/Evaluacion");
 
-#------------------------------------#
 #-----------Uploading the data-------#
-#------------------------------------#
 datos<- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
 head(datos)
 
-#--------------------------------------------#
 #-----  Changing the format - Date-----------#
-#--------------------------------------------#
 install.packages("lubridate")
 library(lubridate)
 install.packages("base")
@@ -26,33 +19,22 @@ datos$Time<- chron(times=datos$Time)
 datos$DateTime <-paste(datos$Date,datos$Time)
 datos$DateTime <-as.POSIXct(datos$DateTime,format="%Y-%m-%d %H:%M:%S")
 
-#-------------------------------------------------------------#
 #----Filter the dates of 2007-02-01 and 2007-02-02 -----------#
-#-------------------------------------------------------------#
 # install.packages("dplyr")
 library(dplyr)
 datos <- filter(datos, datos$Date=="2007-02-01" | datos$Date=="2007-02-02")
 head(datos)
 
-#-------------------------------------------------#
 #-------    Changing Language settings -----------#
-#-------------------------------------------------#
-
-#Extraer la configuración actual ##ORIGINAL: Spanish_Spain.1252
+#Extraer la configuraciÃ³n actual ##ORIGINAL: Spanish_Spain.1252
 original_locale = Sys.getlocale(category = "LC_TIME")
 #Cambiar a formato 'English'
 Sys.setlocale(category = "LC_TIME", locale = "English")
 
-#--------------------------------------------#
 #-------Changing the format of Variables-----#
-#--------------------------------------------#
-
 datos$Global_active_power = as.numeric(as.character(datos$Global_active_power))
 
-#--------------------------------------------#
 #---------------Graphing  -------------------#
-#--------------------------------------------#
-
 png("plot2.png", width=480, height=480)
 plot(x = datos$DateTime
      , y = datos$Global_active_power
@@ -61,6 +43,6 @@ plot(x = datos$DateTime
      , ylab="Global Active Power (kilowatts)")
 dev.off()
 
-#Cambiar de vuelta a la configuración inicial
+#Cambiar de vuelta a la configuraciÃ³n inicial
 Sys.setlocale(category = "LC_TIME", locale = original_locale)
 
